c1a1177ebf6dd5277a399d9d319f76938414e884
Akolade <adesanmiakoladedotun@gmail.com>
2019-05-15 02:24:37 +0100

    feature(mobile-setup): set-up react native mobile app

--- App.js
+++ App.js
@@ -1,21 +1,57 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default class App extends React.Component {
+  state = {
+    bucketList: 'This is an example of mobile bucket list app',
+    add: 0
+  }
+
+  handleChange = (e) => {
+    e.preventDefault()
+    this.setState( prevState => {
+      return {
+        ...prevState,
+        add: prevState.add + 1
+      }
+    })
+  }
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Text style={styles.text}>Start Living Your Bucket List!</Text>
+        <TextInput
+          style={styles.inputStyle}
+        />
+        <Button
+          title="Search"
+          onPress={this.handleChange}
+        />
+        <Text style={styles.textResult}>{this.state.bucketList} {this.state.add}</Text>
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
+const styles = {
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+  inputStyle: {
+    borderColor: 'green',
+    height: 40,
+    borderWidth: 1,
+    width: '90%',
+    marginBottom: 10,
+    padding: 'auto 10px'
+  },
+  text: {
+    paddingBottom: 10
+  },
+  textResult: {
+    paddingTop: 10
+  }
+};
